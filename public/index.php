5eada448d6fbbe515e4e1483085e802d1b59a806
Pavel <pavel.plotnikov1999@mail.ru>
2023-01-31 14:11:45 +0700

    FIX

--- public/index.php
+++ public/index.php
@@ -20,6 +20,18 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->addBodyParsingMiddleware();
 
 $app->post('/', FeedbackController::class);
